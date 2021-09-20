8259ab4363ab1fdb336e508644abc479ee151920
SMalagic <serkanmalagic@hotmail.com>
2021-09-20 16:53:04 +0300

    initial commit

---
+++ Swift_Tabbar_Component_S/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift_Tabbar_Component_S
+//
+//  Created by Serkan Mehmet Malagiç on 20.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
